4eea7c539838086f5706293341ac7f2d62f063d6
Hasanga-Anawarathna <hasangaanawarathna@gmail.com>
2026-02-10 15:10:45 +0530

    feat: Mark transactions component as standalone and add RouterLink import for navigation support

--- src/app/components/transactions/transactions.ts
+++ src/app/components/transactions/transactions.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-transactions',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink],
   templateUrl: './transactions.html',
   styleUrl: './transactions.css',
 })
